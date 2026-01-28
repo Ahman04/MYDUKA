5dc04a71b4b73925017c89d45ec44c5392d2a1bc
Ahman04 <abokey2020@gmail.com>
2026-01-28 14:08:49 +0300

    frontend: scaffold Vite + Tailwind and add basic routing

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        MyDuka Frontend is Ready
+      </h1>
+    </div>
+  );
+}
