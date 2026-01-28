dcdb5b4c7265895d2fa9dfd9e55b31bbd7d90524
Christine Mumbi <christinemumbi286@gmail.com>
2026-01-28 14:27:15 +0300

    Intial commit

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return{
+        "message": "Welcome to MyDuka API",
+        "docs":"/docs",
+        "health":"/health_check"
+    }
+
 @app.get("/health_check")
 async def ping():
     return {"result": "healthy"}
