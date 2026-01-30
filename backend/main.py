f40c4fd6e18ab2fedbdd6d1c7031635ff5e017f6
Christine Mumbi <christinemumbi286@gmail.com>
2026-01-30 13:26:24 +0300

    Added the columns to the database and configured the main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI 
+from app.core.database import engine, Base
+from app.models import user
 
-app = FastAPI()
+
+app = FastAPI(title="MyDuka API")
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"message":"MyDuka backend is running"}
 
 @app.get("/health_check")
 async def ping():
